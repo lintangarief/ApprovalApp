28f1760918f823b2cb5d34057dcd2d99eaf805bd
Arip 41studio <arip@41studio.com>
2015-09-30 12:54:46 +0700

    fix services

---
+++ www/js/services.js
@@ -0,0 +1,92 @@
+var services = angular.module('services', []);
+
+services.factory('ConnectionService', function() {
+  var objService = {
+    check: function() {
+      if(navigator.connection != undefined){
+        var networkState = navigator.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN]  = 1;//'Unknown connection';
+        states[Connection.ETHERNET] = 2;//'Ethernet connection';
+        states[Connection.WIFI]     = 3;//'WiFi connection';
+        states[Connection.CELL_2G]  = 4;//'Cell 2G connection';
+        states[Connection.CELL_3G]  = 5;//'Cell 3G connection';
+        states[Connection.CELL_4G]  = 6;//'Cell 4G connection';
+        states[Connection.CELL]     = 7;//'Cell generic connection';
+        states[Connection.NONE]     = 8;//'No network connection';
+        return states[networkState];
+      }
+    }
+  };
+  return objService;
+});
+
+services.factory('Storage', function(){
+  return {
+    getItem: function (key) {
+      return localStorage.getItem(key);
+    },
+
+    getObject: function (key) {
+      return JSON.parse(localStorage.getItem(key));
+    },
+
+    setItem: function (key, data) {
+      localStorage.setItem(key, data);
+    },
+
+    setObject: function (key, data) {
+      localStorage.setItem(key, JSON.stringify(data));
+    },
+
+    remove: function (key) {
+      localStorage.removeItem(key);
+    },
+
+    clearAll : function () {
+      localStorage.clear();
+    }
+  };
+});
+
+services.factory('UserService', function($q){
+  var objService = {
+    signIn: function(){
+
+    },
+    signUp: function(){
+
+    },
+    changePassword: function(){
+
+    },
+    updateAccount: function(){
+
+    }
+    showAccount: function(){
+
+    }
+  };
+  return objService;
+});
+
+services.factory('', function($q){
+  var objService = {
+    listApprovals: function(){
+
+    },
+    showApproval: function(){
+
+    },
+    markApproval: function(){
+
+    },
+    cancleApproval: function(){
+
+    },
+    listHistoryApprovals: function(){
+
+    }
+  };
+  return objService;
+})
